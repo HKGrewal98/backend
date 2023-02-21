b8cdabf8aa34241201f40933681fd9f453bf32f5
HKGrewal98 <104564558+HKGrewal98@users.noreply.github.com>
2023-02-21 12:46:35 -0500

    CORS issue

--- routes/login.js
+++ routes/login.js
@@ -79,7 +79,7 @@ loginApp.post('/login',passport.authenticate('local'),(req,res)=>{
       req.session.save()
       //res.header('Access-Control-Allow-Origin','http://localhost:3000')
      
-      res.header('Access-Control-Allow-Origin',req.headers.host)
+      res.header('Access-Control-Allow-Origin','http://localhost:3000')
       res.header('Access-Control-Allow-Credentials', 'true');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
