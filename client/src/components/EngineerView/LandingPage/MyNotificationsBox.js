b5e3c62de048d90fa8fdeb53964fe6280c0e5b12
HKGrewal98 <104564558+HKGrewal98@users.noreply.github.com>
2023-03-03 01:26:43 -0500

    FE FInal

--- client/src/components/EngineerView/LandingPage/MyNotificationsBox.js
+++ client/src/components/EngineerView/LandingPage/MyNotificationsBox.js
@@ -17,13 +17,13 @@ export const MyNotificationsBox = () => {
     axios({
      method:'get',
      maxBodyLength: Infinity,
-     url: '/project/notification/engineer',
+     url: '/project/notifications',
      credentials: "include", 
      withCredentials:true,
      headers:myHeaders
     }).then(res=>{
     //  dispatch(LoaderStatus(false))
-    //  console.log("response form MyNotifications Box", res.data)
+     console.log("response form MyNotifications Box", res.data)
     if(res?.data?.data?.length>0){
        setNotificationData(res?.data?.data)
     }
